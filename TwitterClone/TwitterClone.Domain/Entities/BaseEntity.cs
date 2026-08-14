5bbf0c76bc2b9c9d4030ad217ee1209e6a7fb278
shefat2002 <shefat2002@gmail.com>
2026-08-14 19:33:15 +0600

    updated user.cs

--- TwitterClone/TwitterClone.Domain/Entities/BaseEntity.cs
+++ TwitterClone/TwitterClone.Domain/Entities/BaseEntity.cs
@@ -2,15 +2,15 @@ namespace TwitterClone.Domain.Entities;
 
 public class BaseEntity
 {
-    public Guid Id { get; }
+    public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? ModifiedAt { get; set; }
     public Guid CreatedBy { get; set; }
     public Guid? ModifiedBy { get; set; }
     
-    public BaseEntity()
+    public BaseEntity(Guid id)
     {
-        Id = Guid.NewGuid();
+        Id = id;
         CreatedAt = DateTime.UtcNow;
     }
 }
--- TwitterClone/TwitterClone.Domain/Entities/User.cs
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -1,32 +1,15 @@
 namespace TwitterClone.Domain.Entities;
 
-public class User
+public class User : BaseEntity
 {
-    private Guid _id;
-    private string _username;
-    private string _email;
-
-    public Guid Id
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+   
+    public User(string firstName, string lastName, string email) : base(Guid.NewGuid())
     {
-        get { return _id; }
-    }
-
-    public string Username
-    {
-        get { return _username; }
-    }
-    public string Email
-    {
-        get { return _email; }
-    }
-
-    private User()
-    {
-    }
-    public User(Guid id, string username, string email)
-    {
-        _id = id;
-        _username = username;
-        _email = email;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
     }
 }
