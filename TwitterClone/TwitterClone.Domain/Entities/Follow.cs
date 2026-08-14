e9c684603979e2789e596811d1b42bbeaea97d48
shefat2002 <shefat2002@gmail.com>
2026-08-14 19:44:15 +0600

    Update Follow.cs

--- TwitterClone/TwitterClone.Domain/Entities/Follow.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Follow.cs
@@ -1,31 +1,13 @@
 namespace TwitterClone.Domain.Entities;
 
-public class Follow
+public class Follow : BaseEntity
 {
-    private Guid _followerId;
-    private Guid _followeeId;
-    private DateTime _followedAt;
+    public Guid FollowerId { get; set; }
+    public Guid FollowingId { get; set; }
 
-    public Guid FollowerId
+    public Follow(Guid followerId, Guid followingId) : base(Guid.NewGuid())
     {
-        get { return _followerId; }
-    }
-    public Guid FolloweeId
-    {
-        get { return _followeeId; }
-    }
-    public DateTime FollowedAt
-    {
-        get { return _followedAt; }
-    }
-    private Follow()
-    {
-    }
-
-    public Follow(Guid followerId, Guid followeeId)
-    {
-        _followerId = followerId;
-        _followeeId = followeeId;
-        _followedAt = DateTime.UtcNow;
+        FollowerId = followerId;
+        FollowingId = followingId;
     }
 }
