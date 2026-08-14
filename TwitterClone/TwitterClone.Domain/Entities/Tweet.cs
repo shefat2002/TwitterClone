df395ecea28cbcca41b13dcc4f14d40974d39ef6
shefat2002 <shefat2002@gmail.com>
2026-08-14 19:37:32 +0600

    Update Tweet.cs

--- TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
@@ -2,10 +2,10 @@ namespace TwitterClone.Domain.Entities;
 
 public class Tweet : BaseEntity
 {
-    public string AuthorId { get; set; }
+    public Guid AuthorId { get; set; }
     public string Content { get; set; }
     
-    public Tweet(string authorId, string content) : base(Guid.NewGuid())
+    public Tweet(Guid authorId, string content) : base(Guid.NewGuid())
     {
         AuthorId = authorId;
         Content = content;
